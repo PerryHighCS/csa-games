d2d7bcd1afbaff3b4cbc36e5ee2c3ed798d4203a
csarkar373 <csarkar373@gmail.com>
2020-07-20 01:13:36 -0400

    attempt to remove button focus for mobile

--- src/components/GridButton.jsx
+++ src/components/GridButton.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import * as AppConstants from "../util/appconstants";
+import "../css/gridbutton.css";
 
 class GridButton extends Component {
   state = {};

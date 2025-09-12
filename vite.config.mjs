1be2a755a79920e33a0b5a8ceb70d2bd7b2adf43
Brian Dahlem <dahlem.brian@cusd80.com>
2025-09-11 20:27:12 -0700

    refactor: configure vite for jsx

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,9 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
+    esbuild: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
   },
 });
